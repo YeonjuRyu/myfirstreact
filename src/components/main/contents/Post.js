98889b2cb057ae2c39e2cb6add1d089cbac64c8c
david <david@t2nters.com>
2019-08-26 13:29:37 +0900

    code review commit

--- src/components/main/contents/Post.js
+++ src/components/main/contents/Post.js
@@ -23,8 +23,9 @@ class Post extends React.Component{
         // 그런데, 사실 여기에서 parseInt() 등을 사용했다든지, 명확히 구분해서 parse한 것과 parse 하지 않은 것을 분리해서 처리해야 할 때엔
         // parsed id 로 명명하는 것은 좋습니다만. 사실 여기에서는 id 정도로만 하셔도 의미 전달은 충분히 될 것으로 보입니다.
         var parsedid = this.props.match.params.id;
-        // TODO: ks: fetch문의 들여쓰기 정리해 주세요. 32라인에 })는 무엇에 대한 것인지 파악하기 쉽지 않습니다.
+        // TODO: ks: fetch문의 들여쓰기 정리해 주세요. 33라인에 })는 무엇에 대한 것인지 파악하기 쉽지 않습니다.
         // TODO: ks: 중복주소추출: 아래의 input에 들어가는 주소 경로는 별도의 상수로 추출하는 것이 좋습니다.
+        // TODO: 아래의 fetch 문이 정확히 무엇을 하는지 이해하기 쉽지 않습니다. 별도의 method로 추출해 주시면, 의미 전달이 보다 명확하게 될 것입니다.
         fetch('http://127.0.0.1:3000/board/post/'+parsedid)
         .then((res)=>{res.json()
         .then((data)=>{this.setState({post: data.postDetail})
