b1c4f4050e476aa2fe89928d8cfea6dea532af4b
david <david@t2nters.com>
2019-08-26 13:24:13 +0900

    code review commit

--- src/components/main/contents/Post.js
+++ src/components/main/contents/Post.js
@@ -17,12 +17,19 @@ class Post extends React.Component{
 
     componentDidMount(){
         this._isMounted = true;
+        // TODO: ks: 변수명은 camel case를 사용하시거나, underscored variable을 사용하시든 어느 쪽이든 선택하심이 좋을 것 같습니다.
+        // 가독성의 문제와 오해가 일어날 가능성을 줄이기 위해서 입니다.
+        // parsedid는 parsed id로도 parse did로도 보일 수 있기 때문입니다.
+        // 그런데, 사실 여기에서 parseInt() 등을 사용했다든지, 명확히 구분해서 parse한 것과 parse 하지 않은 것을 분리해서 처리해야 할 때엔
+        // parsed id 로 명명하는 것은 좋습니다만. 사실 여기에서는 id 정도로만 하셔도 의미 전달은 충분히 될 것으로 보입니다.
         var parsedid = this.props.match.params.id;
+        // TODO: ks: fetch문의 들여쓰기 정리해 주세요. 32라인에 })는 무엇에 대한 것인지 파악하기 쉽지 않습니다.
+        // TODO: ks: 중복주소추출: 아래의 input에 들어가는 주소 경로는 별도의 상수로 추출하는 것이 좋습니다.
         fetch('http://127.0.0.1:3000/board/post/'+parsedid)
         .then((res)=>{res.json()
         .then((data)=>{this.setState({post: data.postDetail})
         })
-      }) 
+      })
     }
 
     componentDidUpdate(){
@@ -31,7 +38,7 @@ class Post extends React.Component{
         .then((res)=>{res.json()
         .then((data)=>{this.setState({post: data.postDetail})
         })
-      }) 
+      })
     }
 
     componentWillUnmount() {
